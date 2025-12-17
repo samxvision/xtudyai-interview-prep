7e1ea582ad31cda49601915149ee4a4ea522c9bd
Filmi Fun <shortsfest9@gmail.com>
2025-12-17 13:35:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8834937510-cf495",
+  "appId": "1:782363832038:web:a90abce6a587756ce1d527",
+  "apiKey": "AIzaSyDlwky3-HDY6pVOJBYVeTP6n3Ss53XEoRo",
+  "authDomain": "studio-8834937510-cf495.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "782363832038"
+};
